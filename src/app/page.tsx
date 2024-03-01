928671f8cb7ce5c9dba6d9ad7a642f87ad750206
vibhor <vibgitcode27@gmail.com>
2024-03-02 02:53:30 +0530

    added redux/@toolkit for stateManagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
-
 import Image from "next/image";
 import styles from "./page.module.css";
 import { connetToMetmask } from "./ether/ether";
+import { RootState } from "@/lib/store";
+import { useAppDispatch , useAppSelector } from "@/lib/hooks";
+import { increment , decrement } from "@/lib/features/counter/counter";
 
 export default function Home() {
+
+  const count : number = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
     <main className={styles.main}>
       <button onClick={connetToMetmask}> Connect </button>
+
+      <button onClick={() => {dispatch(increment())}}>Increment</button>
+      <span>{count}</span>
     </main>
   );
 }
